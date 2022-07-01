112c9b8d374bd07e894677c36918a8d903b35698
smrdgn <93001578+smrdgn@users.noreply.github.com>
2022-07-01 17:32:54 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,1 @@
-
-for i in range(1,101):
-  
-  if(i%3==0 and i%5==0):
-    print("FizzBuzz")
-  
-  elif(i%3 == 0):
-    print("Fizz")
-  
-  elif(i%5 == 0):
-    print("Buzz")
- 
-  else:
-    print(i)
+for i in range(1, 101): print("Fizz"*(i%3==0)+"Buzz"*(i%5==0) or str(i))
